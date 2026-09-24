f9e2e8d8e802fc45ad651ba52e89d60273db071f
hatrongvu13 <hatrongvu13@gmail.com>
2026-09-24 21:48:23 +0700

    init project spring mqtt sensor ingestion

---
+++ src/main/java/com/htv/lab/mqttingestion/SpringMqttSensorIngestionApplication.java
@@ -0,0 +1,13 @@
+package com.htv.lab.mqttingestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMqttSensorIngestionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMqttSensorIngestionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/htv/lab/mqttingestion/SpringMqttSensorIngestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.htv.lab.mqttingestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMqttSensorIngestionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
